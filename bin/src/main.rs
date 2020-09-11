1d36df9ebf3b79f202e3ef4563e7db02498c7f74
David Craven <david@craven.ch>
2020-09-11 18:16:43 +0200

    Update cli.

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,78 +1,67 @@
-use crate::command::*;
 use clap::Clap;
-use ipfs_embed::{Cid, Config, Metadata, Store, WritableStore};
+use ipfs_embed_db::BlockStore;
 use libipld::block::Block;
+use libipld::cid::Cid;
 use libipld::codec::Codec;
-use libipld::codec_impl::Multicodec;
 use libipld::json::DagJsonCodec;
-use libipld::multihash::Multihash;
+use libipld::store::DefaultStoreParams;
+use std::path::PathBuf;
 
-mod command;
+#[derive(Clone, Debug, Clap)]
+pub struct Opts {
+    #[clap(subcommand)]
+    pub cmd: SubCommand,
+    #[clap(short = "p", long = "path")]
+    pub path: PathBuf,
+    #[clap(short = "t", long = "tree")]
+    pub tree: Option<String>,
+}
+
+#[derive(Clone, Debug, Clap)]
+pub enum SubCommand {
+    Ls,
+    Cat(CidCommand),
+    References(IdCommand),
+    Referrers(IdCommand),
+}
+
+#[derive(Clone, Debug, Clap)]
+pub struct CidCommand {
+    cid: Cid,
+}
+
+#[derive(Clone, Debug, Clap)]
+pub struct IdCommand {
+    id: u64,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let opts = Opts::parse();
-    let db = sled::open(opts.path)?;
-    let tree_name = opts.tree.unwrap_or_else(|| ipfs_embed::TREE.to_string());
-    let tree = db.open_tree(tree_name)?;
-    let config = Config::new(tree, Default::default());
-    let store = Store::<Multicodec, Multihash>::new(config)?;
+    let store = BlockStore::open(&opts.path)?;
     match opts.cmd {
-        SubCommand::Tree => {
-            for name in db.tree_names() {
-                println!("{}", std::str::from_utf8(&name).unwrap());
+        SubCommand::Ls => {
+            for (id, cid, pin) in store.blocks()? {
+                println!("{} {} {}", id, cid.to_string(), pin);
             }
         }
-        SubCommand::Ls(LsCommand {
-            pinned,
-            live,
-            dead,
-            all,
-        }) => {
-            println!(
-                "{:10} {:10} {:10} {:10} cid",
-                "pins", "parents", "children", "public"
-            );
-            for res in store.blocks() {
-                let cid = res?;
-                let metadata = store.metadata(&cid)?;
-                let is_pinned = metadata.pins > 0;
-                let is_live = metadata.referers > 0 || metadata.pins > 0;
-                let all = all || (!pinned && !live && !dead);
-                let print = all || pinned && is_pinned || live && is_live || dead && !is_live;
-                if print {
-                    print_metadata(&cid, &metadata);
-                }
-            }
-        }
-        SubCommand::Cat(CatCommand { cid }) => {
-            if let Some(bytes) = store.get_local(&cid)? {
-                let data = bytes.to_vec().into_boxed_slice();
-                let block = Block::<Multicodec, Multihash>::new(cid, data);
-                let json = DagJsonCodec.encode(&block.decode_ipld()?)?;
+        SubCommand::Cat(CidCommand { cid }) => {
+            if let Some(data) = store.get(&cid)? {
+                let block = Block::<DefaultStoreParams>::new_unchecked(cid, data);
+                let json = DagJsonCodec.encode(&block.ipld()?)?;
                 println!("{}", std::str::from_utf8(&json)?);
             }
         }
-        SubCommand::Refs(RefsCommand { cid }) => {
-            let metadata = store.metadata(&cid)?;
-            for cid in metadata.refs {
-                println!("{}", cid.to_string());
+        SubCommand::References(IdCommand { id }) => {
+            for (id, cid) in store.references(id)? {
+                println!("{} {}", id, cid.to_string());
             }
         }
-        SubCommand::Unpin(UnpinCommand { cid }) => {
-            async_std::task::block_on(store.unpin(&cid))?;
+        SubCommand::Referrers(IdCommand { id }) => {
+            for (id, cid) in store.referrers(id)? {
+                println!("{} {}", id, cid.to_string());
+            }
         }
     }
     Ok(())
-}
-
-fn print_metadata(cid: &Cid, metadata: &Metadata) {
-    println!(
-        "{:10} {:10} {:10} {:10} {}",
-        metadata.pins.to_string(),
-        metadata.referers.to_string(),
-        metadata.refs.len().to_string(),
-        metadata.public,
-        cid.to_string()
-    );
 }
